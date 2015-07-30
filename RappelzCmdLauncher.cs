a19acb220eeb384e626bd5e0160c54c075d73dce
glandu2 <glandu2epvp@gmail.com>
2015-07-30 21:29:05 +0200

    Show MessageBox if no arguments (to match C implementation) and simplify passing SECURITY_ATTRIBUTES to CreateEvent

--- RappelzCmdLauncher.cs
+++ RappelzCmdLauncher.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.IO;
 using System.Diagnostics;
+using System.Windows.Forms;
 using HANDLE = System.IntPtr;
 
 namespace RappelzCmdLauncher
@@ -10,42 +11,36 @@ namespace RappelzCmdLauncher
 	class RappelzCmdLauncher
 	{
 		[DllImport("Kernel32.dll", SetLastError = true, CallingConvention = CallingConvention.Winapi, CharSet = CharSet.Auto)]
-        public static extern HANDLE CreateEvent(HANDLE lpEventAttributes, [In, MarshalAs(UnmanagedType.Bool)] bool bManualReset, [In, MarshalAs(UnmanagedType.Bool)] bool bIntialState, [In, MarshalAs(UnmanagedType.BStr)] string lpName);
+		public static extern HANDLE CreateEvent(ref SECURITY_ATTRIBUTES lpEventAttributes, [In, MarshalAs(UnmanagedType.Bool)] bool bManualReset, [In, MarshalAs(UnmanagedType.Bool)] bool bIntialState, [In, MarshalAs(UnmanagedType.BStr)] string lpName);
 
 		[DllImport("Kernel32.dll", SetLastError = true)]
 		static extern UInt32 WaitForSingleObject(IntPtr hHandle, UInt32 dwMilliseconds);
 
 		[StructLayout(LayoutKind.Sequential)]
-        public struct SECURITY_ATTRIBUTES {
-            public int nLength;
-            public IntPtr lpSecurityDescriptor;
-            public int bInheritHandle;
-        }
-
-		static IntPtr securityAttributeToPtr(SECURITY_ATTRIBUTES attrib) {
-			int sz = Marshal.SizeOf(attrib.GetType());
-			IntPtr ptr = Marshal.AllocHGlobal(sz);
-			Marshal.StructureToPtr(attrib, ptr, false);
-
-			return ptr;
+		public struct SECURITY_ATTRIBUTES {
+			public int nLength;
+			public IntPtr lpSecurityDescriptor;
+			public int bInheritHandle;
 		}
 
 		static HANDLE createEvent()
 		{
-			SECURITY_ATTRIBUTES securityAttrib;
+			SECURITY_ATTRIBUTES securityAttrib = new SECURITY_ATTRIBUTES();
 
 			securityAttrib.bInheritHandle = 1;
 			securityAttrib.lpSecurityDescriptor = IntPtr.Zero;
 			securityAttrib.nLength = Marshal.SizeOf(typeof(SECURITY_ATTRIBUTES));
 
-			IntPtr securityAttribPtr = securityAttributeToPtr(securityAttrib);
-			return CreateEvent(securityAttribPtr, false, false, String.Empty);
+			return CreateEvent(ref securityAttrib, false, false, String.Empty);
 		}
 
 		static void Main(string[] args)
 		{
-			if (args.Length < 2)
+			if (args.Length < 1)
+			{
+				MessageBox.Show("Error: not enough arguments\nUsage: RappelzCmdLauncher.exe SFrame.exe ...", "RappelzCmdLauncher ~ glandu2", MessageBoxButtons.OK, MessageBoxIcon.Error);
 				return;
+			}
 
 			ProcessStartInfo startInfo = new ProcessStartInfo();
 			HANDLE eventHandle = createEvent();
